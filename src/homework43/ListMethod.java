92c140cce139578826069e4568a5e5149e883ddf
Shmahailo <yulifeya@ukr.netgit cleargit config --global user.email yulifeya@ukr.net>
2025-03-27 21:12:33 +0100

    homework34

--- src/homework43/ListMethod.java
+++ src/homework43/ListMethod.java
@@ -11,18 +11,6 @@ import java.util.Collection;
 import java.util.List;
 
 public class ListMethod {
-    public static List<Integer> findCommonElements(Collection<Integer> listA, Collection<Integer> listB) {
-        List<Integer> result = new ArrayList<>();
-
-        for (Integer elementA : listA) {
-            if (listB.contains(elementA) && !result.contains(elementA)) {
-                result.add(elementA);
-            }
-
-        }
-        return result;
-    }
-
 
         public static void main(String[] args) {
         Collection<Integer> collectionA = new ArrayList<>();
@@ -30,8 +18,8 @@ public class ListMethod {
         collectionA.addAll(List.of(10, 20, -10, 30, 20, 40, 50, 60));
         collectionB.addAll(List.of(20, 30, 40, 100, 80, 50, -10));
 
-            List<Integer> commonElements = findCommonElements(collectionA, collectionB);
-            System.out.println("Common elements: " + commonElements);
+            collectionA.retainAll(collectionB);
+            System.out.println(collectionA);
         }
 
 
